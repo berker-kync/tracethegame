60d58e7f7667a347708f9e4b8d8bc3b294be36c2
Berker Koyuncu <126168041+berker-kync@users.noreply.github.com>
2026-02-20 20:08:01 -0500

    Room, character, movement mechanics, collision mechanics created

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,18 @@
 extends CharacterBody2D
 
-
-const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 150.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+	
+	velocity = direction.normalized() * SPEED
 	move_and_slide()
