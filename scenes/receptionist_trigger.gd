02f2f87b8229d19c5833f97dc267819c08ac33fe
elise-handley <elisehandley5@gmail.com>
2026-03-10 23:32:19 -0400

    Working Text box interaction
    still no working journal

--- scenes/receptionist_trigger.gd
+++ scenes/receptionist_trigger.gd
@@ -1,18 +1,25 @@
 extends Area2D
+
 var proximity = false
-#@onready var highlighted: Sprite2D = $highlighted
-#@onready var game_manager: Node = $"Game Manager"
+@onready var textBox = $"../textBox"
+
+
+func _ready():
+	textBox.visible = false
+
 
 func _process(delta: float) -> void:
-	if proximity == true && Input.is_action_just_pressed("interact"):
-		print("interacted") 
+	if proximity and Input.is_action_just_pressed("interact"):
+		textBox.visible = true
+		print("interacted")
+
 
 func _on_body_entered(_body: Node2D) -> void:
-	#highlighted.visible = true
 	proximity = true
 	print("entered")
-	
+
+
 func _on_body_exited(_body: Node2D) -> void:
-	#highlighted.visible = false
 	proximity = false
+	textBox.visible = false
 	print("exited")
