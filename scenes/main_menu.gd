84aa434ac820899ad77771278f069378c98e7b81
elise-handley <elisehandley5@gmail.com>
2026-03-21 22:43:53 -0400

    main menu
    all buttons work and are linked to appropriate scenes. only work that needs doing is updated assets.

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
 func _on_play_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file(str("res://scenes/Lobby/HotelLobby.tscn"))
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	$CenterContainer/MainButtons.visible = false
+	$CenterContainer/Settings.visible = true
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	$CenterContainer/MainButtons.visible = false
+	$CenterContainer/Credits.visible = true
 
 func _on_quit_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
+
+func _on_back_pressed() -> void:
+	$CenterContainer/Credits.visible = false
+	$CenterContainer/Settings.visible = false
+	$CenterContainer/MainButtons.visible = true
