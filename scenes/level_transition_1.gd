af81df133a77799e3069ec45bc1bbdebfc263d07
Caladin759 <caleb.m.akins@gmail.com>
2026-03-30 12:06:42 -0400

    stuff

--- scenes/level_transition_1.gd
+++ scenes/level_transition_1.gd
@@ -3,7 +3,7 @@ extends Area2D
 #@onready var game_manager: Node = $"Game Manager"
 @onready var transition: AnimationPlayer = $"../../Transition"
 var proximity = false
-var demoRoom = preload("res://scenes/Elise Room 2/demoRoom.tscn")
+var demoRoom = load("res://scenes/Elise Room 2/demoRoom.tscn")
 
 func _process(delta: float) -> void:
 	if proximity == true && Input.is_action_just_pressed("interact"):
@@ -11,19 +11,15 @@ func _process(delta: float) -> void:
 		transition.play("Fade_out")
 		#level transition
 
-
-
 func _on_body_entered(_body: Node2D) -> void:
 	#highlighted.visible = true
 	proximity = true
 	print("entered") 
 	
-	
 func _on_body_exited(_body: Node2D) -> void:
 	#highlighted.visible = false
 	proximity = false
 	print("exited")
-
 
 func _on_transition_animation_finished(anim_name: StringName) -> void:
 	if anim_name != "Fade_in":
