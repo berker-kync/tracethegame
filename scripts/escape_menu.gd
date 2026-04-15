098aa31659dc5d7a34a667f583d132e7ebe6ce1e
elise-handley <elisehandley5@gmail.com>
2026-04-15 13:12:47 -0400

    trying to fix escape menu

--- scripts/escape_menu.gd
+++ scripts/escape_menu.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
 func _ready() -> void:
-	$CenterContainer.visible = false
+	$CenterContainer.visible = true
 	
 	$CenterContainer/Credits.visible = false
 	$CenterContainer/Settings.visible = false
 	$CenterContainer/MainButtons.visible = true
 
 
-func _input(event):
-	if event.is_action_pressed("Escape"):
-		toggle_menu()
+#func _input(event):
+#	if event.is_action_pressed("Escape"):
+#		toggle_menu()
 
 
 func toggle_menu():
@@ -24,7 +24,6 @@ func toggle_menu():
 		$CenterContainer/Credits.visible = false
 		$CenterContainer/Settings.visible = false
 		$CenterContainer/MainButtons.visible = true
-
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Lobby/HotelLobby.tscn")
@@ -40,11 +39,11 @@ func _on_credits_pressed() -> void:
 	$CenterContainer/Credits.visible = true
 
 
-func _on_quit_pressed() -> void:
-	get_tree().quit()
-
-
 func _on_back_pressed() -> void:
 	$CenterContainer/Credits.visible = false
 	$CenterContainer/Settings.visible = false
 	$CenterContainer/MainButtons.visible = true
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
