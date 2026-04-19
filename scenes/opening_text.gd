2875082968cc7dbe1a547dbddb18bc0db9fd1f33
Caladin759 <caleb.m.akins@gmail.com>
2026-04-19 15:39:03 -0400

    openingpicture

--- scenes/opening_text.gd
+++ scenes/opening_text.gd
@@ -6,7 +6,7 @@ func _ready():
 	# Queue all dialogue immediately when scene loads
 	textBox.queueText("Welcome to the TRACE: The Game.")
 	textBox.queueText("You are here on a work visit as a Digital Footprint Detective.")
-	textBox.queueText("You can move using either WASD or Arrow Keys.")
+	textBox.queueText("You can move using either WASD or Arrow Keys, and use Enter to interact.")
 	textBox.queueText("Checkout the lobby, make yourself comfortable.")
 	textBox.queueText("When you're ready, talk to the receptionist by walking up to the desk and pressing Enter.")
 	
