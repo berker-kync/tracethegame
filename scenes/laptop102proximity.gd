a17652c4929027c2e956d323a35c578f532fcc0d
elise-handley <elisehandley5@gmail.com>
2026-04-22 11:17:13 -0400

    Laptop screen opens on interaction

--- scenes/laptop102proximity.gd
+++ scenes/laptop102proximity.gd
@@ -1,10 +1,12 @@
 extends Area2D
 @onready var highlighted: Sprite2D = $"../highlighted"
+var laptopScene = load("res://scenes/laptop.tscn")
 var proximity = false
 
 func _process(delta: float) -> void:
 	if proximity == true && Input.is_action_just_pressed("interact"):
 		print("interacted") 
+		get_tree().change_scene_to_packed(laptopScene)
 
 func _on_body_entered(_body: Node2D) -> void:
 	highlighted.visible = true
