99816d3e58d5eb12facfec75b8fb9532479920aa
elise-handley <elisehandley5@gmail.com>
2026-04-21 22:30:07 -0400

    Working laptop screen and watch password

--- scripts/passwordfailtext.gd
+++ scripts/passwordfailtext.gd
@@ -3,12 +3,9 @@ extends Node2D
 @onready var textBox = $".."
 
 func _ready():
-	# Queue all dialogue immediately when scene loads
 	textBox.queueText("It looks like you failed the password too many times.")
 	textBox.queueText("Maybe there is more evidence somewhere else in the room.")
 	textBox.queueText("When you're ready, come back and try again.")
 
-	
-	# Wait until dialogue is finished, then load lobby
-	await textBox.finished  
-	get_tree().change_scene_to_file("res://scenes/room_102.tscn")
+#	await textBox.finished  
+	#get_tree().change_scene_to_file("res://scenes/room_102.tscn")
--- scripts/smile.gd
+++ scripts/smile.gd
@@ -1,9 +1,23 @@
 extends Area2D
 
+@onready var smile: Area2D = $"."
+#@onready var smilesgram: Area2D = $"../Area2D"
+@onready var background: Sprite2D = $"../Background"
+@onready var post_it: Sprite2D = $"../Post-it"
+@onready var settings: Area2D = $"../Settings"
+@onready var smilesgram: Area2D = $"../Smilesgram"
+
+
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			print("Clicked on: Smile")
+			smile.visible = false
+			background.visible = false
+			post_it.visible = false
+			settings.visible = false
+			smilesgram.visible = true
+			
 
 func _on_mouse_entered() -> void:
 	pass # Replace with function body.
