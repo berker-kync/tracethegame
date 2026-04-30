cfbd3a00e286d8c5a2a45aa6d0521ed128f0aa2f
Caladin759 <caleb.m.akins@gmail.com>
2026-04-30 10:58:26 -0400

    1

--- scripts/gamer_notepad.gd
+++ scripts/gamer_notepad.gd
@@ -4,11 +4,7 @@ extends Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	textbox.queueText("Looks like they have left their passwords easily accessible!")
-	textbox.queueText("This is very common.. many keep their password on the notes app in their phone!")
-	textbox.queueText("Let's solve the puzzle to discover more.")
-	textbox.queueText("Drag the passwords to the correct side on the right page.")
-
+	pass
 
 
 
