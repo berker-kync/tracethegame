57f555242b508058c83800e686750744a45ed51f
elise-handley <elisehandley5@gmail.com>
2026-05-03 23:07:37 -0400

    reciept fix

--- scripts/recieptinfo.gd
+++ scripts/recieptinfo.gd
@@ -1,18 +1,18 @@
 extends Panel
 
 @onready var main_cam: Camera2D = $"../mainCam"
-@onready var button: Button = $Button2
+#@onready var button: Button = $Button2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	button.hide()
+	#button.hide()
 	await get_tree().create_timer(5.0).timeout
-	button.show()
+	#button.show()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
-func _on_button_2_pressed() -> void:
-	main_cam.make_current()
+#func _on_button_2_pressed() -> void:
+#	main_cam.make_current()
 	
