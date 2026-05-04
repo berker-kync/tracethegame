dfadeec9c97883168b1fc6f09d8d7865d2aba579
elise-handley <elisehandley5@gmail.com>
2026-05-03 22:58:02 -0400

    journal adjustments

--- scripts/panel_container_1.gd
+++ scripts/panel_container_1.gd
@@ -1,18 +1,19 @@
 extends PanelContainer
-@onready var panel_container: PanelContainer = $"."
-@onready var phone_description: Sprite2D = $"../../tab3/PhoneDescription"
+@onready var notebook_description: Sprite2D = $"../NotebookDescription"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
 func _on_mouse_entered() -> void:
-	if (journalConst.room3keyItem2 == true):
-		phone_description.visible = true
+	if (journalConst.room3keyItem1 == true):
+		notebook_description.visible = true
+
 
 func _on_mouse_exited() -> void:
-		phone_description.visible = false
+		notebook_description.visible = false
--- scripts/recieptinfo.gd
+++ scripts/recieptinfo.gd
@@ -15,3 +15,4 @@ func _process(delta: float) -> void:
 
 func _on_button_2_pressed() -> void:
 	main_cam.make_current()
+	
